034364f3b09737d38c495a0c4867321520751a97
Maxheart <samuelhenrykurniawan@yahoo.com>
2021-04-10 15:02:43 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import OSS from './pages';
+
+function App() {
+  return <OSS />;
+}
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import HomePage from "./HomePage";
+
+function OSS() {
+    return <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Redirect to="/" />
+        </Switch>
+    </BrowserRouter>
+}
+
+export default OSS;
