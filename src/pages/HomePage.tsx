0ded60f1f746c9fb205dab5294a90c067820eed3
Maxheart <samuelhenrykurniawan@yahoo.com>
2021-04-10 15:56:43 +0800

    add prettier and eslint

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function HomePage() {
-    return <div>Hi, this is the home page</div>;
+  return <div>Hi, this is the home page</div>;
 }
 
 export default HomePage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-import HomePage from "./HomePage";
+import HomePage from './HomePage';
 
 function OSS() {
-    return <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Redirect to="/" />
-        </Switch>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Redirect to="/" />
+      </Switch>
     </BrowserRouter>
+  );
 }
 
 export default OSS;
